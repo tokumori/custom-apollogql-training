cfa14f2eda6d1cd4e9d37468a143ec31daed664d
Bryce <bsaito88@gmail.com>
2018-10-18 14:24:08 -1000

    Connected `graphql` and `graphiql` endpoints.

--- server/src/server.js
+++ server/src/server.js
@@ -4,10 +4,14 @@ import cors from 'cors';
 import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
 
 import db from '../models'
-import schema from './schema'
+import {schema} from './schema'
 
 let app = express();
 app.use('*', cors());
+app.use('/graphql', bodyParser.json(), graphqlExpress( { schema }));
+app.use('/graphiql', graphiqlExpress({
+  endpointURL: '/graphql',
+}));
 
 const PORT = 4000;
 
